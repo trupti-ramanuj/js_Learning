0c1d2b334809b3dc6f7a28abc739a2e67a2dd16a
trupti-ramanuj <trupti.r@devstree.in>
2026-08-11 05:56:40 +0000

    add

---
+++ 01_basics/dataType.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3);
+
+let name ="trupti"
+let age =18
+let isLoggedIn=false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof age); // object
