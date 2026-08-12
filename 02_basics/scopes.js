9c023a5762ad777cf66b80434687a038e4ed6720
trupti-ramanuj <trupti.r@devstree.in>
2026-08-12 07:43:04 +0000

    add

---
+++ 02_basics/scopes.js
@@ -0,0 +1,12 @@
+var c = 300 //global scope
+let a = 300
+if (true) {     //block scope
+    let a = 10
+    const b = 20
+    var c = 30
+    console.log("INNER: ", a);
+}
+console.log(a);
+//console.log(b);
+console.log(c);
+
