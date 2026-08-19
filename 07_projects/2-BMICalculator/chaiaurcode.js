d696de96fd18648841cc9110b36ab29db7cffaf1
trupti-ramanuj <trupti.r@devstree.in>
2026-08-19 10:10:16 +0000

    add

--- 07_projects/2-BMICalculator/chaiaurcode.js
+++ 07_projects/2-BMICalculator/chaiaurcode.js
@@ -12,19 +12,32 @@ form.addEventListener('submit', function (e) {
     } else if (weight === '' || weight < 0 || isNaN(weight)) {
         result.innerHTML = `please give a valid weight ${weight}`
     }
+    else if (weight < 18.6) {
+        weight.innerHTML = `Under Weight ${result}`
+    } else if (weight === 18.6 || weight <= 24.9) {
+        weight.innerHTML = `Normal Range ${result}`
+    }
+    else if (weight > 24.9) {
+        weight.innerHTML = `Overweight ${result}`
+    }
+
     else {
         const bmi = (weight / ((height * height) / 10000)).toFixed(2)
 
         //show the result
         results.innerHTML = `<span>${bmi}</span>`
-    }
 
-    // if (result < 18.6) {
-    //     result.innerHTML = `Under Weight ${result}`
-    // } else if (result === 18.6 || result <= 24.9) {
-    //     result.innerHTML = `Normal Range ${result}`
+    }
+    //     if (weight < 18.6) {
+    //         weight.innerHTML = `Under Weight ${result}`
+    //     } else if (weight === 18.6 ||  <= 24.9) {
+    //         weight.innerHTML = `Normal Range ${result}`
+    //     }
+    //     else {
+    //         weight.innerHTML = `Overweight ${result}`
+    //     }
     // }
-    // else {
-    //     result.innerHTML = `Overweight ${result}`
-    // }
-})
+
+
+
+});
