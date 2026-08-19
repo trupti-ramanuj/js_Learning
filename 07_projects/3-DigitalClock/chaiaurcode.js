cc37b253bf4dca3d7f5575088855d50db8839652
trupti-ramanuj <trupti.r@devstree.in>
2026-08-19 10:35:37 +0000

    add

---
+++ 07_projects/3-DigitalClock/chaiaurcode.js
@@ -0,0 +1,10 @@
+const clock = document.getElementById('clock')
+// document.querySelector('#clock')
+
+// let date = new Date()
+// console.log(date.toLocaleTimeString());
+
+setInterval(function () {
+    let date = new Date();
+    clock.innerHTML = date.toLocaleTimeString();
+}, 1000);
